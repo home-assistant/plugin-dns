b133b27c00b63622af800c1da84e2e2b5cfe9e72
Pascal Vizeli <pvizeli@syshack.ch>
2020-10-28 11:26:46 +0000

    Add fallback

--- configurator/main.go
+++ configurator/main.go
@@ -14,9 +14,14 @@ func main() {
 	// parse command lines
 	flag.Parse()
 
+	// Get config
 	config := readConfigFile(*configFile)
-	data := renderCoreFile(config, *coreFile)
 
+	// Add fallback
+	config.Locals = append(config.Locals, "dns://127.0.0.11")
+
+	// create & write corefile
+	data := renderCoreFile(config, *coreFile)
 	err := ioutil.WriteFile(*outFile, data, 0644)
 	if err != nil {
 		log.Fatal(err)
