376acd88201ed80e73c9e2679ec4c1dd8ae11c34
Pascal Vizeli <pvizeli@syshack.ch>
2020-10-28 10:34:58 +0000

    Fix template

--- configurator/template.go
+++ configurator/template.go
@@ -3,12 +3,16 @@ package main
 import (
 	"bytes"
 	"log"
+	"strings"
 	"template"
 )
 
 func renderCoreFile(config *CoreDNSConfig, coreFile string) []byte {
 	buf := &bytes.Buffer{}
 	coreTemplate := template.Must(template.ParseFile(coreFile))
+
+	// Add functions
+	coreTemplate.Funcs(template.FuncMap{"Join": strings.Join})
 
 	err := coreTemplate.Execute(buf, config)
 	if err != nil {
