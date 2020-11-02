89671d1c9d756645a2f120d776d5dfe267326400
Pascal Vizeli <pvizeli@syshack.ch>
2020-11-02 10:08:41 +0100

    Don't break on readonly filesystem (#14)

--- rootfs/etc/cont-init.d/corefile.sh
+++ rootfs/etc/cont-init.d/corefile.sh
@@ -11,7 +11,7 @@ if [ ! -f /config/coredns.json ]; then
     bashio::exit.ok
 elif [ -f /config/corefile ]; then
     bashio::log.info "Cleanup old corefile"
-    rm -f /config/corefile
+    rm -f /config/corefile || true
 fi
 
 # Generate corefile
