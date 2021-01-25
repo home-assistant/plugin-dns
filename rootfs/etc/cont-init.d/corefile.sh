24e6641064a38befdf8e10a0819bc727bc12f9fb
Pascal Vizeli <pvizeli@syshack.ch>
2021-01-25 14:46:30 +0000

    Migrate to new tempio

--- rootfs/etc/cont-init.d/corefile.sh
+++ rootfs/etc/cont-init.d/corefile.sh
@@ -3,22 +3,11 @@
 # Handle corefile generation
 # ==============================================================================
 
-# MIGRATION: can removed later
-if [ ! -f /config/coredns.json ]; then
-    bashio::log.warning "coredns.json not found, using supervisor configuration"
-    cp -f /config/corefile /etc/corefile
-
-    bashio::exit.ok
-elif [ -f /config/corefile ]; then
-    bashio::log.info "Cleanup old corefile"
-    rm -f /config/corefile || true
-fi
-
 # Generate corefile
 if ! tempio \
         -conf /config/coredns.json \
-        -template /usr/share/corefile.tempio \
+        -template /usr/share/tempio/corefile \
         -out /etc/corefile
 then
-    bashio::log.error "Corefile fails to generate. Use fallback corefile"
+    bashio::log.error "Corefile fails to generate. Use fallback corefile!"
 fi
