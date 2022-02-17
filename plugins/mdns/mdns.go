67502667780f0a7dcf6f32589de9964d4100829d
Mike Degatano <michael.degatano@gmail.com>
2022-02-16 20:24:43 -0500

    Using systemd resolver via dbus

--- plugins/mdns/mdns.go
+++ plugins/mdns/mdns.go
@@ -1,15 +1,16 @@
 package mdns
 
 import (
+	"net"
 	"strings"
-	"sync"
-	"time"
+	"syscall"
 
 	"github.com/coredns/coredns/plugin"
+	"github.com/coredns/coredns/plugin/mdns/dbusgen"
 	clog "github.com/coredns/coredns/plugin/pkg/log"
 	"github.com/coredns/coredns/request"
 
-	"github.com/grandcat/zeroconf"
+	"github.com/godbus/dbus/v5"
 	"github.com/miekg/dns"
 	"golang.org/x/net/context"
 )
@@ -17,35 +18,44 @@ import (
 var log = clog.NewWithPlugin("mdns")
 
 type MDNS struct {
-	Next      plugin.Handler
-	mutex     *sync.RWMutex
-	mdnsHosts *map[string]*zeroconf.ServiceEntry
+	conn     *dbus.Conn
+	Next     plugin.Handler
+	Resolver *dbusgen.Org_Freedesktop_Resolve1_Manager
 }
 
-func (m MDNS) AddARecord(msg *dns.Msg, state *request.Request, hosts map[string]*zeroconf.ServiceEntry, name string) bool {
+func (m MDNS) AddARecord(ctx context.Context, msg *dns.Msg, state *request.Request, name string) bool {
 	// Add A and AAAA record for name (if it exists) to msg.
 	// A records need to be returned in A queries, this function
 	// provides common code for doing so.
-	answerEntry, present := hosts[name]
-	if present {
-		if answerEntry.AddrIPv4 != nil && state.QType() == dns.TypeA {
-			aheader := dns.RR_Header{Name: name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60}
-			// TODO: Support multiple addresses
-			msg.Answer = append(msg.Answer, &dns.A{Hdr: aheader, A: answerEntry.AddrIPv4[0]})
-		}
-		if answerEntry.AddrIPv6 != nil && state.QType() == dns.TypeAAAA {
-			aaaaheader := dns.RR_Header{Name: name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 60}
-			msg.Answer = append(msg.Answer, &dns.AAAA{Hdr: aaaaheader, AAAA: answerEntry.AddrIPv6[0]})
-		}
-		return true
+	addresses, _, _, err := m.Resolver.ResolveHostname(ctx, 0, name, syscall.AF_UNSPEC, 0)
+
+	if err != nil {
+		log.Error("Failed to reach systemd resolver: ", err)
+		return false
 	}
-	return false
+
+	resolved := false
+	for i := 0; i < len(addresses); i++ {
+		addr := addresses[i]
+		if addr.V1 == syscall.AF_INET && state.QType() == dns.TypeA {
+			ip := net.IPv4(addr.V2[0], addr.V2[1], addr.V2[2], addr.V2[3])
+			aheader := dns.RR_Header{Name: name, Rrtype: dns.TypeA, Class: dns.ClassINET, Ttl: 60}
+			msg.Answer = append(msg.Answer, &dns.A{Hdr: aheader, A: ip})
+			resolved = true
+
+		} else if addr.V1 == syscall.AF_INET6 && state.QType() == dns.TypeAAAA {
+			var ip net.IP = addr.V2
+			aaaaheader := dns.RR_Header{Name: name, Rrtype: dns.TypeAAAA, Class: dns.ClassINET, Ttl: 60}
+			msg.Answer = append(msg.Answer, &dns.AAAA{Hdr: aaaaheader, AAAA: ip})
+			resolved = true
+		}
+	}
+	return resolved
 }
 
 func (m MDNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (int, error) {
 	msg := new(dns.Msg)
 	state := request.Request{W: w, Req: r}
-	mdnsHosts := *m.mdnsHosts
 	hostName := strings.ToLower(state.QName())
 
 	// Prepare message
@@ -64,10 +74,7 @@ func (m MDNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (i
 
 	msg.Answer = []dns.RR{}
 
-	m.mutex.RLock()
-	defer m.mutex.RUnlock()
-
-	if m.AddARecord(msg, &state, mdnsHosts, hostName) {
+	if m.AddARecord(ctx, msg, &state, hostName) {
 		log.Debug(msg)
 		w.WriteMsg(msg)
 		return dns.RcodeSuccess, nil
@@ -77,84 +84,18 @@ func (m MDNS) ServeDNS(ctx context.Context, w dns.ResponseWriter, r *dns.Msg) (i
 	return plugin.NextOrFailure(m.Name(), m.Next, ctx, w, r)
 }
 
-func (m *MDNS) BrowseMDNS() {
-	entriesSrv := make(chan *zeroconf.ServiceEntry)
-	mdnsHosts := make(map[string]*zeroconf.ServiceEntry)
-	discovery := []string{}
-
-	// Retrieve Services
-	go func(results <-chan *zeroconf.ServiceEntry) {
-		log.Debug("Retrieving mDNS services")
-		for entry := range results {
-			serviceName := strings.TrimSuffix(entry.Instance, ".local")
-			log.Debugf("Service: %s\n", serviceName)
-			discovery = append(discovery, serviceName)
-		}
-	}(entriesSrv)
-
-	// Get all available services
-	queryService("_services._dns-sd._udp", entriesSrv, 10)
-
-	// Discover hosts
-	for _, serviceName := range discovery {
-		processService(serviceName, mdnsHosts)
-
-		// Update fast the list to get soon a answer
-		for k, v := range mdnsHosts {
-			if _, found := (*m.mdnsHosts)[k]; !found {
-				(*m.mdnsHosts)[k] = v
-			}
-		}
+func (m MDNS) ConnectDBus() error {
+	conn, err := dbus.ConnectSystemBus()
+	if err != nil {
+		return err
 	}
-
-	m.mutex.Lock()
-	defer m.mutex.Unlock()
-	// Clear maps so we don't have stale entries
-	for k := range *m.mdnsHosts {
-		delete(*m.mdnsHosts, k)
-	}
-	// Copy values into the shared maps only after we've collected all of them.
-	// This prevents us from having to lock during the entire mdns browse time.
-	for k, v := range mdnsHosts {
-		(*m.mdnsHosts)[k] = v
-	}
+	m.conn = conn
+	m.Resolver = dbusgen.NewOrg_Freedesktop_Resolve1_Manager(conn.Object("org.freedesktop.resolve1", "/org/freedesktop/resolve1"))
+	return nil
 }
 
-func processService(service string, mdnsHosts map[string]*zeroconf.ServiceEntry) {
-	entriesHost := make(chan *zeroconf.ServiceEntry)
-
-	// Retrieve Hosts
-	go func(results <-chan *zeroconf.ServiceEntry) {
-		log.Debug("Retrieving mDNS entries")
-		for entry := range results {
-			// Make a copy of the entry so zeroconf can't later overwrite our changes
-			localEntry := *entry
-			if localEntry.HostName != "" {
-				log.Debugf("Instance: %s, HostName: %s, AddrIPv4: %s, AddrIPv6: %s\n", localEntry.Instance, localEntry.HostName, localEntry.AddrIPv4, localEntry.AddrIPv6)
-				mdnsHosts[strings.ToLower(localEntry.HostName)] = entry
-			} else {
-				log.Debugf("Ignore Instance: %v", localEntry)
-			}
-		}
-	}(entriesHost)
-
-	queryService(service, entriesHost, 8)
-}
-
-func queryService(service string, channel chan *zeroconf.ServiceEntry, timeout int) {
-	resolver, err := zeroconf.NewResolver(nil)
-	if err != nil {
-		log.Errorf("Failed to initialize %s resolver: %s", service, err.Error())
-		return
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout) * time.Second)
-	defer cancel()
-	err = resolver.Browse(ctx, service, "local.", channel)
-	if err != nil {
-		log.Errorf("Failed to browse %s records: %s", service, err.Error())
-		return
-	}
-	<-ctx.Done()
+func (m MDNS) Disconnect() error {
+	return m.conn.Close()
 }
 
 func (m MDNS) Name() string { return "mdns" }
--- plugins/mdns/setup.go
+++ plugins/mdns/setup.go
@@ -1,32 +1,26 @@
 package mdns
 
 import (
-	"sync"
-	"time"
-
 	"github.com/coredns/caddy"
 	"github.com/coredns/coredns/core/dnsserver"
 	"github.com/coredns/coredns/plugin"
-
-	"github.com/grandcat/zeroconf"
 )
 
 func init() { plugin.Register("mdns", setup) }
 
 func setup(c *caddy.Controller) error {
-	mdnsHosts := make(map[string]*zeroconf.ServiceEntry)
-	mutex := sync.RWMutex{}
-	m := MDNS{mutex: &mutex, mdnsHosts: &mdnsHosts}
-
-	for c.Next() {
-		if c.NextArg() {
-			return plugin.Error("mdns", c.ArgErr())
-		}
+	m := MDNS{}
+	c.Next()
+	if c.NextArg() {
+		return plugin.Error("mdns", c.ArgErr())
 	}
 
 	c.OnStartup(func() error {
-		go browseLoop(&m)
-		return nil
+		return m.ConnectDBus()
+	})
+
+	c.OnShutdown(func() error {
+		return m.Disconnect()
 	})
 
 	dnsserver.GetConfig(c).AddPlugin(func(next plugin.Handler) plugin.Handler {
@@ -35,11 +29,4 @@ func setup(c *caddy.Controller) error {
 	})
 
 	return nil
-}
-
-func browseLoop(m *MDNS) {
-	for {
-		m.BrowseMDNS()
-		time.Sleep(120 * time.Second)
-	}
 }
